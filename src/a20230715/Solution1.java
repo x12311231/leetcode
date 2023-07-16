f28d1ff77237f922f417ff2f7d3e98d78fe4e085
ubuntu22.04LTS <xhqpro@outlook.com>
2023-07-16 13:41:35 +0800

    ...

--- src/a20230715/Solution1.java
+++ src/a20230715/Solution1.java
@@ -1,10 +1,45 @@
 package a20230715;
 
-import java.util.List;
+import java.util.*;
 
 public class Solution1 implements C{
     @Override
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        List<List<Integer>> lists = new ArrayList<>();
+        Map<String, Boolean> map = new HashMap<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    List<Integer> list = new ArrayList<>();
+                    long search =  ((long) target - nums[i] - nums[j] - nums[k]);
+                    long[] arr = new long[]{nums[i], nums[j], nums[k], search};
+                    Arrays.sort(arr);
+                    String key = String.valueOf(arr[0])+ arr[1] + arr[2] + search;
+                    if (map.containsKey(key)) {
+                        continue;
+                    }
+                    int left = k + 1;
+                    int right = nums.length - 1;
+                    while (left <= right) {
+                        int middle = (left + right) / 2;
+                        if (search > nums[middle]) {
+                            left = middle + 1;
+                        } else if (search < nums[middle]) {
+                            right = middle - 1;
+                        } else {
+                            map.put(key, true);
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            list.add((int) search);
+                            lists.add(list);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return lists;
     }
 }
--- src/a20230715/Test.java
+++ src/a20230715/Test.java
@@ -1,14 +1,33 @@
 package a20230715;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        s1(new Solution());
+//        s1(new Solution());
+        s1(new Solution1());
+//        testScope();
+//        testOverflowInt();
+//        testOverflowLong();
+//        System.out.println(Integer.MAX_VALUE >= Math.pow(10, 9));
+    }
+
+    public static void testScope() {
+       int i = 1000000000 + 1000000000 + 1000000000 + 1000000000;
+        int iu = - 1000000000 - 1000000000 - 1000000000 - 1000000000;
+        long l = 1000000000 + 1000000000 + 1000000000 + 1000000000;
+        long ll = 1000000000 + 100000000 + 100000000 + 100000000;
+        int ii = 1000000000;
+        int iii = (int) Math.pow(10, 9);
+        int i1 = (int) Math.pow(10, 2);
+        Double d = Math.pow(10, 9);
+        System.out.println(d.MAX_VALUE);
     }
 
     public static void s1(C solution) {
         System.out.println(solution.getClass());
+        assert1(solution.fourSum(new int[]{1000000000,1000000000,1000000000,1000000000}, -294967296), -294967296, 0);
         assert1(solution.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0), 0, 3);
         assert1(solution.fourSum(new int[]{1, 0, -1, 1, 0, -2, 2}, 0), 0, 4);
         assert1(solution.fourSum(new int[]{2, 2, 2, 2, 2}, 8), 8, 1);
@@ -20,10 +39,119 @@ public class Test {
             for (Object ii: l1) {
                 sum += (Integer) ii;
             }
-            System.out.println(sum == target);
+            boolean x = sum == target;
+            System.out.println("assert:" + x);
+            System.out.println("sum:" + sum);
+            if (!x) {
+                System.out.println("l1:" + l1);
+            }
         }
-        System.out.println(lists.size() == expectedSize);
+        System.out.println("expected size:" + (lists.size() == expectedSize));
+//        if (lists.size() != expectedSize) {
+//            System.out.println("expectedSize:" + expectedSize + " lists.size():" + lists.size());
+//        }
     }
+
+    public static void testOverflowInt() {
+        System.out.println(isOverflow(new int[]{2147483647, 1, 1}));
+        System.out.println(isOverflow(new int[]{2147483647, 2, 1}));
+//        System.out.println(isOverflow(new int[]{1000000000,1000000000,1000000000,1000000000}));
+    }
+
+    public static void testOverflowLong() {
+        System.out.println(isOverflow(new long[]{1000000000,1000000000,1000000000,1000000000}));
+        System.out.println(isOverflow(new long[]{2147483647, 1, 1}));
+        System.out.println(isOverflow(new long[]{2147483647, 2, 1}));
+    }
+
+    public static void testOverflowByte() {
+        boolean res;
+//        res = isOutOfBounds(new byte[]{50, 50});
+//        System.out.println(res == true);
+//
+
+        res = isOverflow(new byte[]{-50, -50, -50});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{50, 50, 50});
+        System.out.println(res == false);
+
+        res = isOverflow(new byte[]{50, 50, -50, 50});
+        System.out.println(res == true);
+        res = isOverflow(new byte[]{50, 70, -50, 70});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{50, 70, -50, 70, -50});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{-100, -27, -1, 5, 10});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{-100, -27, 5, 10});
+        System.out.println(res == true);
+        res = isOverflow(new byte[]{-100, -27, 5, 10, 100, 100});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{100, 28, -5, -10, -100, -100});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{-100, -27, 100, 28, -27, -100, -1});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{100, 28, -100, -27, 28, 100, 1});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{100, 28, -100, -27, -1});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{-100, -27, 100, 27, 1, 1});
+        System.out.println(res == false);
+        res = isOverflow(new byte[]{-100, -27, -100, -27, -1, -1});
+//        res = isOutOfBounds(new int[]{-128});
+//        System.out.println(res == false);
+//        res = isOutOfBounds(new int[]{129});
+//        System.out.println(res == false);
+//        res = isOutOfBounds(new int[]{1290});
+//        System.out.println(res == false);
+//        res = isOutOfBounds(new int[]{-1290});
+//        System.out.println(res == false);
+    }
+
+    /**
+     *
+     * @param arr every int <= 128 && >= -127
+     * @return
+     */
+    public static boolean isOverflow(byte[] arr) {
+//        return isOutOfBounds(arr, -127, 128);
+        Stream.of(arr).forEach(System.out::print);
+        int max = 128;
+        int min = -127;
+        byte sum = 0;
+        for (byte a: arr) {
+            sum += a;
+//            if (sum > 0 && a > 0) {
+//
+//            }
+        }
+        System.out.println(sum);
+        return true;
+    }
+    public static boolean isOverflow(int[] arr) {
+        Stream.of(arr).forEach(System.out::print);
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int a: arr) {
+            sum += a;
+        }
+        System.out.println(sum);
+        return true;
+    }
+    public static boolean isOverflow(long[] arr) {
+        Stream.of(arr).forEach(System.out::print);
+        long max = Long.MAX_VALUE;
+        long min = Long.MIN_VALUE;
+        long sum = 0;
+        for (long a: arr) {
+            sum += a;
+        }
+        System.out.println(sum);
+        return true;
+    }
+//    public static boolean isOutOfBounds(int[] arr, int min, int max) {
+//    }
 }
 //[-494,-487,-471,-470,-465,-462,-447,-445,-441,-432,-429,-422,-406,-398,-397,-364,-344,-333,-328,-307,-302,-293,-291,-279,-269,-269,-268,-254,-198,-181,-134,-127,-115,-112,-96,-94,-89,-58,-58,-58,-44,-2,-1,43,89,92,100,101,106,106,110,116,143,156,168,173,192,231,248,256,281,316,321,327,346,352,353,355,358,365,371,410,413,414,447,473,473,475,476,481,491,498]
 //        8511
@@ -48,7 +176,9 @@ public class Test {
 //
 //            输入：nums = [2,2,2,2,2], target = 8
 //            输出：[[2,2,2,2]]
-//
+///[1000000000,1000000000,1000000000,1000000000]
+//        -294967296
+//        /
 //
 //
 //            提示：
